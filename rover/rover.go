faf25f0b12c45ed815a667aa28a3e878f1f3377d
Quentin JUVET <juvet.quentin19@gmail.com>
2024-10-15 12:47:39 +0200

    fix

--- rover/rover.go
+++ rover/rover.go
@@ -1,5 +1,7 @@
 package rover
 
+import "fmt"
+
 type Rover interface {
 	Move(step int)
 	Turn(direction string)
@@ -15,4 +17,46 @@ type RoverImpl struct {
 	Orientation  string
 	PlanetWidth  int
 	PlanetHeight int
+}
+
+func (r *RoverImpl) Turn(direction string, findIndex func([]string, string) int) {
+	idx := findIndex(directions, r.Orientation)
+
+	if direction == "L" {
+		idx = (idx + 3) % 4
+	} else if direction == "R" {
+		idx = (idx + 1) % 4
+	}
+
+	r.Orientation = directions[idx]
+}
+
+func (r *RoverImpl) Move(step int) {
+	switch r.Orientation {
+	case "N":
+		r.Y = (r.Y - step + r.PlanetHeight) % r.PlanetHeight
+	case "S":
+		r.Y = (r.Y + step) % r.PlanetHeight
+	case "E":
+		r.X = (r.X + step) % r.PlanetWidth
+	case "W":
+		r.X = (r.X - step + r.PlanetWidth) % r.PlanetWidth
+	}
+}
+
+func (r *RoverImpl) ExecuteCommand(command string, findIndex func([]string, string) int) {
+	switch command {
+	case "L":
+		r.Turn("L", findIndex)
+	case "R":
+		r.Turn("R", findIndex)
+	case "M":
+		r.Move(1)
+	case "B":
+		r.Move(-1)
+	}
+}
+
+func (r *RoverImpl) PrintState() {
+	fmt.Printf("Rover position: (%d, %d), Orientation: %s\n", r.X, r.Y, r.Orientation)
 }
